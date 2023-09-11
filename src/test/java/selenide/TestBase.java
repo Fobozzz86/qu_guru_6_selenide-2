b04c148a948a7ae2fa767dbf490951ce620c3118
Artem Bu <fobozzz@yandex.ru>
2023-09-12 01:40:35 +0300

    45 минут проработал

---
+++ src/test/java/selenide/TestBase.java
@@ -0,0 +1,17 @@
+package selenide;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+      //  Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+      // Configuration.browser = "Edge";
+      //  Configuration.browserSize = "700x800";
+
+      //  Configuration.timeout = 30000;
+      //  Configuration.pageLoadTimeout = 45000; // таймаут на загрузку страници
+    }
+}
